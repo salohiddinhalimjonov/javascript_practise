7030d77d73c8ffa08123a367cdc8f4f99c38675e
Salohiddin Halimjonov <00009998id@gmail.com>
2024-04-23 10:48:29 +0500

    initial commit

---
+++ js/script.js
@@ -0,0 +1,90 @@
+"use strict";
+
+// && = va, || = yoki
+const isAge = true;
+const isClose = false
+console.log(isAge && isClose); //false
+console.log(isAge && !isClose); //true
+console.log(isAge || isClose); // true
+
+//console.log(6 - 'hi'); // NaN
+// console.log(6 + "9");
+// console.log(6 + +"9");
+// console.log(6 + +"hello");
+
+// let incr = 10;
+// let decr = 10;
+
+// incr++;
+// decr--;
+// console.log(incr);//11
+// console.log(decr);//9
+
+// console.log(incr++);//11
+// console.log(decr--);//9
+// console.log(incr);//12
+// console.log(decr);//8
+// console.log(++incr);//13
+// console.log(--decr);//7
+
+//console.log(5*5 == "25"); //true
+//console.log(5*5 === "25"); //false
+// const channel = prompt("What is your favourite bloger?", "samarbadriddinov");
+// console.log(`https://youtube.com/${channel}`);
+
+// const user = "Samar";
+// const job = "Java Script Developer";
+// console.log(`User name is ${user}, he is ${job}`);
+// alert("Hello Java Script");
+// const place = confirm("Are you here");
+// console.log(place);
+// const currentPlace = prompt("Where are you?", "Samara");
+// console.log(typeof currentPlace);
+// const age = +prompt("How old are you?", "13");
+// console.log(age + 10);
+// console.log(typeof age);
+// const favouriteColors = [];
+// favouriteColors[0] = prompt('What is your favourite color?', '');
+// favouriteColors[1] = prompt('What is your favourite color?', '');
+// favouriteColors[2] = prompt('What is your favourite color?', '');
+// console.log(favouriteColors);
+// let number = 10;
+// const borderColor = "red";
+// number = 20;
+// console.log(number);
+// const person = {
+//     name: "Samar",
+// };
+// person.name = "Yusuf";
+// console.log(person);
+
+// Number
+// let number = 5.6;
+// console.log(-8/0); // Infinity
+// console.log("Samar"*8); //NaN
+
+// String
+// const clientName = "Samar";
+
+// Boolean
+// const isMarried = false;
+// const isCircleEarth = true;
+
+// null
+// console.log(clientAge);
+
+// undefined
+// let und;
+// console.log(und);
+
+// object
+// const thief = {
+//     age: 30, //number
+//     jacket: "black", //string
+//     isLong: false //boolean
+// };
+// console.log(thief.age);
+
+// array
+// const colors = ["red", "black", "green", 10, [], {}];
+// console.log(colors[1]);
